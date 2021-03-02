b599e9f3109cf23041a55df62c722044aea56989
Dawid <chcezostacprogramista@gmail.com>
2021-03-02 18:58:45 +0100

    Aplikacja Sygnalizacja Świetlna

---
+++ SygnalizacjaSwietlna/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace SygnalizacjaSwietlna
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Sygnalizator MojSygnalizator;
+        System.Timers.Timer Timer;
+
+        public MainWindow()
+        {
+            MojSygnalizator = new Sygnalizator();
+            InitializeComponent();
+            ZmienSwiatloCoOkresCzasu();
+            this.DataContext = MojSygnalizator;
+        }
+
+        private void ZmienSwiatloCoOkresCzasu()
+        {
+            Timer = new System.Timers.Timer();
+            Timer.Elapsed += CzasUplynal;
+            Timer.Interval = 20000;
+            Timer.Enabled = true;
+        }
+
+        private void CzasUplynal(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            MojSygnalizator.ZmienSwiatlo();
+        }
+    }
+}
---
+++ SygnalizacjaSwietlna/Sygnalizator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading;
+using System.Windows.Media;
+using System.Runtime.CompilerServices;
+
+namespace SygnalizacjaSwietlna
+{
+    public class Sygnalizator : INotifyPropertyChanged
+    {
+        public Sygnalizator()
+        {
+            SwiatloGorne = Brushes.Red;
+            SwiatloSrodkowe = Brushes.Transparent;
+            SwiatloDolne = Brushes.Transparent;
+
+            SwiatlaSaZmieniane = false;
+            UstawieniaSygnalizatora = SygnalizatorPozycjaSwiatelEnum.CzerwoneSwiatlo;
+        }
+
+        private Brush _swiatloGorne;
+        private Brush _swiatloSrodkowe;
+        private Brush _swiatloDolne;
+
+        public Brush SwiatloGorne
+        {
+            get
+            {
+                return _swiatloGorne;
+            }
+            set
+            {
+                if (value != _swiatloGorne)
+                {
+                    _swiatloGorne = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public Brush SwiatloSrodkowe
+        {
+            get
+            {
+                return _swiatloSrodkowe;
+            }
+            set
+            {
+                if (value != _swiatloSrodkowe)
+                {
+                    _swiatloSrodkowe = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public Brush SwiatloDolne
+        {
+            get
+            {
+                return _swiatloDolne;
+            }
+            set
+            {
+                if (value != _swiatloDolne)
+                {
+                    _swiatloDolne = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private bool SwiatlaSaZmieniane { get; set; }
+        private SygnalizatorPozycjaSwiatelEnum UstawieniaSygnalizatora { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void ZmienSwiatlo()
+        {
+            if (SwiatlaSaZmieniane == false)
+            {
+                switch (UstawieniaSygnalizatora)
+                {
+                    case SygnalizatorPozycjaSwiatelEnum.CzerwoneSwiatlo:
+                        SwiatlaSaZmieniane = true;
+
+                        SwiatloGorne = Brushes.Red;
+                        SwiatloSrodkowe = Brushes.Yellow;
+                        SwiatloDolne = Brushes.Transparent;
+                        Thread.Sleep(1000);
+                        SwiatloGorne = Brushes.Transparent;
+                        SwiatloSrodkowe = Brushes.Transparent;
+                        SwiatloDolne = Brushes.Green;
+
+                        UstawieniaSygnalizatora = SygnalizatorPozycjaSwiatelEnum.ZieloneSwiatlo;
+                        SwiatlaSaZmieniane = false;
+                        break;
+                    case SygnalizatorPozycjaSwiatelEnum.ZieloneSwiatlo:
+                        SwiatlaSaZmieniane = true;
+
+                        SwiatloGorne = Brushes.Transparent;
+                        SwiatloSrodkowe = Brushes.Yellow;
+                        SwiatloDolne = Brushes.Transparent;
+                        Thread.Sleep(500);
+                        SwiatloGorne = Brushes.Red;
+                        SwiatloSrodkowe = Brushes.Transparent;
+                        SwiatloDolne = Brushes.Transparent;
+
+                        UstawieniaSygnalizatora = SygnalizatorPozycjaSwiatelEnum.CzerwoneSwiatlo;
+                        SwiatlaSaZmieniane = false;
+                        break;
+                }
+            }
+        }
+    }
+}
